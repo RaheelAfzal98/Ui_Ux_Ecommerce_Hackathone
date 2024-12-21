6d4dc532ee6f45b436e22de9f97abba554ee7094
RaheelAfzal98 <infotime8123@gmail.com>
2024-12-21 15:33:40 +0500

    Hackathon complete successfully

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+
+function Home() {
+  return (
+    <div className='w-full-2xl mx-auto'>
+
+      <Hero/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default Home;
